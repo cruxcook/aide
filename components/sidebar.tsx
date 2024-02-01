27153c50a3eb50f01eadc19f51fca31a48d62b68
Crux Cook <cruxcook@tutanota.com>
2024-02-01 12:08:52 -0800

    feat(sidebar): create Dashboard Route
    Issue #4

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,7 @@
 "user client";
 
 import { cn } from "@/lib/utils";
+import { LayoutDashboard } from "lucide-react";
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,6 +10,15 @@ const montserratFont = Montserrat({
   weight: "600",
   subsets: ["latin"],
 });
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+];
 
 const Sidebar = () => {
   return (
@@ -22,6 +32,20 @@ const Sidebar = () => {
             Aide
           </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
