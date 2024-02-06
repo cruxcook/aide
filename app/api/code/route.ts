1ebc31c2af2df7ce3c4ae22085af1c13cdc70e05
Crux Cook <cruxcook@tutanota.com>
2024-02-06 11:46:18 -0800

    feat(code-api): add Default Instruction
    Create & add default instruction message to establish Code Generation
    Behavior.
    
    Issue #13

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,11 +1,18 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { OpenAI } from "openai";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 
 const openai = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"],
 });
 
+// Default message for Code Generation Behavior
+const instructionMessage: ChatCompletionMessageParam = {
+  role: "system",
+  content:
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+};
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
@@ -26,12 +33,12 @@ export async function POST(req: Request) {
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
-      messages,
+      messages: [instructionMessage, ...messages],
     });
 
     return NextResponse.json(response.choices[0].message);
   } catch (error) {
-    console.log("[CONVERSATION_ERROR]", error);
+    console.log("[CODE_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
